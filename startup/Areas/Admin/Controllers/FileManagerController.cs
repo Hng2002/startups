5db6bd7661bc08072cc6efa9e3154c47a0aba6c9
nguyenhong <nguyenthihong28082002@gmail.com>
2023-11-28 17:39:53 +0700

    Add project files.

---
+++ startup/Areas/Admin/Controllers/FileManagerController.cs
@@ -0,0 +1,19 @@
+﻿using elFinder.NetCore.Drivers.FileSystem;
+using elFinder.NetCore;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace startup.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("/Admin/file-manager")]
+    public class FileManagerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
---
+++ startup/Utilities/Functions.cs
@@ -0,0 +1,14 @@
+﻿namespace startup.Utilities
+{
+    public class Functions
+    {
+        public static string TitleSlugGeneration(string type, string title, long id) 
+        {
+            return type + "-" + SlugGenerator.SlugGenerator.GenerateSlug(title) + "-" + id.ToString() + ".abcd";
+        }
+         public static string getCurrentDate()
+        {
+            return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+    }
+}
